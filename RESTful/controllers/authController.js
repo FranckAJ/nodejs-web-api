e71b55ddfc3a381f8a3cc7048dfcc5a680f29b23
Franck Aragão <fjunior.aragao@gmail.com>
2017-01-01 20:35:54 -0300

    generatingtoken of access JWT

--- RESTful/controllers/authController.js
+++ RESTful/controllers/authController.js
@@ -1,10 +1,26 @@
 let User = require('../models/user');
+let jwt = require('jsonwebtoken');
+var HTTPStatus = require('http-status');
+
+const JWT_KEY = 'lets go';
+
 
 exports.authenticate = function(req, res, next) {
-
-	User.forge(req.body.email)
+	var user;
+	User.forge({username: req.body.username})
 		.fetch({require: true})
-		.then((user) => user.compare(req.body.password))
-		.then((isValid) => res.json({login: isValid}))
-		.catch((err) => next(err))
+		.then((result) => {
+			 user = result;
+			 return result.compare(req.body.password);
+		})
+		.then((isValid) => {
+			console.log(isValid);
+			if(isValid){
+				res.json({
+					token: jwt.sign({username: user.username}, JWT_KEY)
+				})
+			}
+		})
+		.catch(res.send("invalid username", HTTPStatus.UNAUTHORIZED))
 };
+
